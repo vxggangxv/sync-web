3582533bd9e759204ca65de3a3a5d48d0efa0bbf
jun <jun@doflab.com>
2022-02-06 23:46:13 +0900

    update

--- src/components/test/TestDetail.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TestItem(props) {
-  return <div>TestItem</div>;
-}
-
-export default TestItem;
--- src/components/test/TestList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function TestList(props) {
-  return <div>TestList</div>;
-}
-
-export default TestList;
